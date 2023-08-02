7553c5bc6582e6bd4624f93e31636e0286534fad
Harold Cheng <niuchangcun@gmail.com>
2023-08-02 23:36:01 +0800

    backend(serializer): support array

--- backend/utils/serializer.go
+++ backend/utils/serializer.go
@@ -4,19 +4,29 @@ import (
 	"bytes"
 	"encoding/binary"
 	"errors"
+	"fmt"
 	"reflect"
 )
 
 // For now, serializer can only serialize centain basic types
 // refer to design doc for more info
 
-// string and byte types must have a `len_bytes` struct tag
-// e.g. B []byte `len_bytes:"2"`
+// string and byte types must have a `len_bytes` or `array_len_bytes` struct tag
+// e.g.
+// B []byte `len_bytes:"2"`
+// C [][]byte `len_bytes:"2" array_len_bytes:"1"`
 
 // fields to be serialized must be exported
 // since package reflect can only change exported fields' values
 
 type Char uint8
+
+type len_type string
+
+const (
+	array_len_bytes len_type = "array_len_bytes"
+	len_bytes       len_type = "len_bytes"
+)
 
 // Marshal can accept both an object or a pointer
 func Marshal(obj any) ([]byte, error) {
@@ -49,25 +59,50 @@ func Marshal(obj any) ([]byte, error) {
 				err = buf.WriteByte(0x00)
 			}
 		case reflect.String:
-			err = writeLen(&field, &value, buf)
-			if err != nil {
-				return nil, err
-			}
+			err = writeString(&field, &value, buf)
 
-			_, err = buf.Write([]byte(value.String()))
-
-		// []bytes
 		case reflect.Slice:
-			if field.Type.Elem().Kind() != reflect.Uint8 {
+			switch field.Type.Elem().Kind() {
+			case reflect.Uint8:
+				// []byte
+				err = writeBytes(&field, &value, buf)
+			case reflect.String:
+				// []string
+				len_arr := value.Len()
+				err = writeLen(&field, &value, buf, "array_len_bytes", len_arr)
+				if err != nil {
+					return nil, err
+				}
+				for i := 0; i < len_arr; i++ {
+					v := value.Index(i)
+					err = writeString(&field, &v, buf)
+					if err != nil {
+						break
+					}
+				}
+
+			case reflect.Slice:
+				// [][]byte
+				// FIXME: very confusing...
+				if field.Type.Elem().Elem().Kind() != reflect.Uint8 {
+					return nil, errors.New("unhandled type")
+				}
+
+				len_arr := value.Len()
+				err = writeLen(&field, &value, buf, "array_len_bytes", len_arr)
+				if err != nil {
+					return nil, err
+				}
+				for i := 0; i < len_arr; i++ {
+					v := value.Index(i)
+					err = writeBytes(&field, &v, buf)
+					if err != nil {
+						break
+					}
+				}
+			default:
 				return nil, errors.New("unhandled type")
 			}
-
-			err = writeLen(&field, &value, buf)
-			if err != nil {
-				return nil, err
-			}
-
-			_, err = buf.Write(value.Bytes())
 
 		default:
 			err = errors.New("unhandled type")
@@ -81,26 +116,41 @@ func Marshal(obj any) ([]byte, error) {
 	return buf.Bytes(), nil
 }
 
-func writeLen(field *reflect.StructField, value *reflect.Value, buf *bytes.Buffer) (err error) {
-	len_bytes_str := field.Tag.Get("len_bytes")
+func writeString(field *reflect.StructField, value *reflect.Value, buf *bytes.Buffer) error {
+	err := writeLen(field, value, buf, "len_bytes", len(value.String()))
+	if err != nil {
+		return err
+	}
+
+	_, err = buf.Write([]byte(value.String()))
+	return err
+}
+
+func writeBytes(field *reflect.StructField, value *reflect.Value, buf *bytes.Buffer) error {
+	err := writeLen(field, value, buf, "len_bytes", len(value.Bytes()))
+	if err != nil {
+		return err
+	}
+
+	_, err = buf.Write(value.Bytes())
+	return err
+}
+
+func writeLen(field *reflect.StructField, value *reflect.Value, buf *bytes.Buffer, tag len_type, length int) (err error) {
+	len_v := length
+	len_bytes_str := field.Tag.Get(string(tag))
 	if len_bytes_str != "1" && len_bytes_str != "2" {
-		err = errors.New("invalid `len_bytes` struct tag")
+		err = fmt.Errorf("invalid `%v` struct tag", tag)
 	} else {
-		var len_v int
-		if field.Type.Kind() == reflect.String {
-			len_v = len(value.String())
-		} else {
-			len_v = len(value.Bytes())
-		}
 		if len_bytes_str == "1" {
 			if len_v > 255 || len_v < 0 {
-				err = errors.New("string with len_bytes 1 must have length <= 255")
+				err = fmt.Errorf("string with %v 1 must have length <= 255", tag)
 			} else {
 				err = buf.WriteByte(uint8(len_v))
 			}
 		} else {
 			if len_v > 65535 || len_v < 0 {
-				err = errors.New("string with len_bytes 1 must have length <= 65535")
+				err = fmt.Errorf("string with %v 1 must have length <= 65535", tag)
 			} else {
 				err = binary.Write(buf, binary.LittleEndian, uint16(len_v))
 			}
@@ -156,25 +206,52 @@ func Unmarshal(b []byte, obj any) error {
 				err = errors.New("unexpected byte")
 			}
 		case reflect.String:
-			data, err := readData(&field, &value, buf)
-			if err != nil {
-				return err
-			}
+			err = readString(&field, &value, buf)
 
-			value.SetString(string(data))
-
-		// []bytes
 		case reflect.Slice:
-			if field.Type.Elem().Kind() != reflect.Uint8 {
-				return errors.New("unhandled type")
-			}
+			switch field.Type.Elem().Kind() {
+			case reflect.Uint8:
+				// []byte
+				err = readBytes(&field, &value, buf)
+			case reflect.String:
+				// []string
+				var length int
+				length, err = readLen(&field, buf, array_len_bytes)
+				if err != nil {
+					return err
+				}
+				for i := 0; i < length; i++ {
+					v := reflect.ValueOf(new(string)).Elem()
+					err = readString(&field, &v, buf)
+					if err != nil {
+						break
+					}
+					value.Set(reflect.Append(value, v))
+				}
+			case reflect.Slice:
+				// [][]byte
+				// FIXME: very confusing
+				if field.Type.Elem().Elem().Kind() != reflect.Uint8 {
+					return errors.New("unhandled type")
+				}
 
-			data, err := readData(&field, &value, buf)
-			if err != nil {
-				return err
+				var length int
+				length, err = readLen(&field, buf, array_len_bytes)
+				if err != nil {
+					return err
+				}
+				for i := 0; i < length; i++ {
+					v := reflect.ValueOf(new([]byte)).Elem()
+					fmt.Println(v.CanAddr())
+					err = readBytes(&field, &v, buf)
+					if err != nil {
+						break
+					}
+					value.Set(reflect.Append(value, v))
+				}
+			default:
+				err = errors.New("unhandled type")
 			}
-
-			value.SetBytes(data)
 
 		default:
 			err = errors.New("unhandled type")
@@ -188,11 +265,30 @@ func Unmarshal(b []byte, obj any) error {
 	return nil
 }
 
-func readData(field *reflect.StructField, value *reflect.Value, buf *bytes.Buffer) (data []byte, err error) {
-	len_bytes_str := field.Tag.Get("len_bytes")
-	var length int
+func readString(field *reflect.StructField, value *reflect.Value, buf *bytes.Buffer) error {
+	data, err := readData(field, value, buf)
+	if err != nil {
+		return err
+	}
+
+	value.SetString(string(data))
+	return nil
+}
+
+func readBytes(field *reflect.StructField, value *reflect.Value, buf *bytes.Buffer) error {
+	data, err := readData(field, value, buf)
+	if err != nil {
+		return err
+	}
+
+	value.SetBytes(data)
+	return nil
+}
+
+func readLen(field *reflect.StructField, buf *bytes.Buffer, tag len_type) (length int, err error) {
+	len_bytes_str := field.Tag.Get(string(tag))
 	if len_bytes_str != "1" && len_bytes_str != "2" {
-		err = errors.New("invalid `len_bytes` struct tag")
+		err = fmt.Errorf("invalid `%v` struct tag", tag)
 	} else {
 		if len_bytes_str == "1" {
 			var data uint8
@@ -204,6 +300,14 @@ func readData(field *reflect.StructField, value *reflect.Value, buf *bytes.Buffe
 			length = int(data)
 		}
 	}
+	if err != nil {
+		return 0, err
+	}
+	return length, nil
+}
+
+func readData(field *reflect.StructField, value *reflect.Value, buf *bytes.Buffer) (data []byte, err error) {
+	length, err := readLen(field, buf, "len_bytes")
 	if err != nil {
 		return nil, err
 	}
